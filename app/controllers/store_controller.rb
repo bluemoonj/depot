c1d503cc2999e0ac254f4038e86f4e7558602373
Paul Lin <paul@uteste.com>
2012-12-11 12:05:01 -0800

    chapter 11 finished.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,14 @@ class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
   	@page_title = "Store Bookshelf"
+  	
+  	@cart = current_cart
+  	
+  	# increase access counter
+  	if session[:counter].nil?
+  		session[:counter] = 1
+  	else
+  		session[:counter] += 1
+  	end
   end
 end
